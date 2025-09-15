58237eae6caf198d725ecfcd397b4d6f8f2d87bd
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-09-15 17:36:20 +0530

    [INJIMOB-3499]: Update Tests to increase code coverage and QR code fallback updated (#9)
    * [INJIMOB-3499]]: Making fields public for visility in native modules
    
    [INJIMOB-3499]: Changing visibility modes for fields in exception
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3499]: Update fallback Qr code base64
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3499]: Write tests for Network Manager
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3499]: QrCode generator tests updated
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3499]: Update image id during qr code fallback scenario
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    * [INJIMOB-3499]: Update readme
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>
    
    ---------
    
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>

--- Sources/InjiVcRenderer/constants/CredentialFormat.swift
+++ Sources/InjiVcRenderer/constants/CredentialFormat.swift
@@ -2,7 +2,7 @@ public enum CredentialFormat: String {
     case ldp_vc = "ldp_vc"
     case unknown = "unknown"
 
-    static func fromValue(_ value: String) -> CredentialFormat {
+    public static func fromValue(_ value: String) -> CredentialFormat {
         return CredentialFormat(rawValue: value) ?? .unknown
     }
 }
